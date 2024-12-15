fd98e91fbc755f2a1334b920c0708e8aef209e2f
Bernardo Morais <bernardomalfredo@hotmail.com>
2024-12-14 23:37:55 -0300

    Fix blank screen error

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,7 +4,11 @@ import { createInertiaApp } from "@inertiajs/vue3";
 createInertiaApp({
     resolve: (name) => {
         const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
-        return pages[`./Pages/${name}.vue`];
+        const importPage = pages[`./Pages/${name}.vue`];
+        if(!importPage) {
+            throw new Error(`Not found ${name}`);
+        }
+        return importPage;
     },
     setup({ el, App, props, plugin }) {
         createApp({ render: () => h(App, props) })
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return inertia('Inertia/Index');
+    return inertia('Index/Index');
 });
